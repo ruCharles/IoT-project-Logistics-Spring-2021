dac6106feb7aab573aa72bc8485b958cea95eba2
ruCharles <42855687+ruCharles@users.noreply.github.com>
2021-05-01 11:21:23 -0500

    Fixed serial terminal issues

--- smart_inventory.ino
+++ smart_inventory.ino
@@ -11,7 +11,7 @@
 
 #define RST_PIN         22           // Configurable, see typical pin layout above
 #define SS_PIN          21          // Configurable, see typical pin layout above
-#define RETRASO       2000
+#define RETRASO       250
 
 void entrada_producto();
 void salida_producto();
@@ -20,6 +20,7 @@ void mostrar_menu();
 void limpiar_terminal();
 void mfrc_write();
 void mfrc_read();
+byte serial_echo( byte * data);
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance
 //*****************************************************************************************//
@@ -31,12 +32,18 @@ void setup() {
 }
 
 //*****************************************************************************************//
-
-
 //Esta función muestra un menú de acción que se actualiza cada que se termina la acción solicitada
 void loop()
 {
   mostrar_menu();
+
+/**
+  byte  data[30]= {};
+  byte len;
+  len = serial_echo(data);
+
+  Serial.println(len);
+*/
 }
 
 void mostrar_menu()
@@ -54,14 +61,17 @@ void mostrar_menu()
     Serial.println(" 3. Grabar info de producto ");
     Serial.println(" - - - - - - - - - - - - - - - - - - - - - - -");
 
-    Serial.flush(); //flush all previous received and transmitted data
+    
     while(!Serial.available()) ; // hang program until a byte is received notice the ; after the while()
 
-    tarea_elegida =  Serial.read();
+    tarea_elegida = Serial.peek();
+    Serial.println( tarea_elegida - 48 );
+    Serial.flush(); //flush all previous received and transmitted data
 
     switch (tarea_elegida)
     {
-    case '1':
+      case '1':
+      
         entrada_producto();
         break;
     case '2':
@@ -78,6 +88,7 @@ void mostrar_menu()
 }
 void entrada_producto()
 {
+    limpiar_terminal();
     Serial.println("Registrando ENTRADA del producto...");
     mfrc_read();
     
@@ -92,7 +103,7 @@ void salida_producto()
 
 void grabar_caducidad()
 {
-    Serial.println("Introduzca fecha de caducidad");
+    //Serial.println("Introduzca fecha de caducidad");
 
     mfrc_write();
     
@@ -133,6 +144,9 @@ void mfrc_write()
   MFRC522::PICC_Type piccType = mfrc522.PICC_GetType(mfrc522.uid.sak);
   Serial.println(mfrc522.PICC_GetTypeName(piccType));
 
+ 
+  limpiar_terminal();
+
   byte buffer[34];
   byte block;
   MFRC522::StatusCode status;
@@ -140,8 +154,12 @@ void mfrc_write()
 
   Serial.setTimeout(20000L) ;     // wait until 20 seconds for input from serial
   // Introduce la caducidad del producto
-  Serial.println(F(" Escribe la fecha de caducidad del producto y termina con un punto . "));
-  len = Serial.readBytesUntil('.', (char *) buffer, 30) ; // read family name from serial
+  Serial.println(F(" Ingresa NOMBRE o SKU del producto. "));
+
+  len = serial_echo( buffer);
+  //len = Serial.readBytesUntil('.', (char *) buffer, 30) ; // read family name from serial
+
+
   for (byte i = len; i < 30; i++) buffer[i] = ' ';     // pad with spaces
 
   block = 1;
@@ -181,9 +199,14 @@ void mfrc_write()
   }
   else Serial.println(F("MIFARE_Write() success: "));
 
+ 
+  limpiar_terminal();
+
   // Pregunta el nomnbre del producto
-  Serial.println(F(" Escribe el nombre del producto o SKU y termina con un punto . "));
-  len = Serial.readBytesUntil('.', (char *) buffer, 20) ; // read first name from serial
+  Serial.println(F(" Ingresa la fecha de caducidad "));
+    //len = Serial.readBytesUntil('.', (char *) buffer, 20) ; // read first name from serial
+
+  len =  serial_echo( buffer);
   for (byte i = len; i < 20; i++) buffer[i] = ' ';     // pad with spaces
 
   block = 4;
@@ -251,7 +274,7 @@ void mfrc_read()
     return;
   }
 
-  Serial.println(F("**Card Detected:**"));
+ // Serial.println(F("**Card Detected:**"));
 
   //-------------------------------------------
 
@@ -260,8 +283,13 @@ void mfrc_read()
   //mfrc522.PICC_DumpToSerial(&(mfrc522.uid));      //uncomment this to see all blocks in hex
 
   //-------------------------------------------
+  limpiar_terminal();
 
-  Serial.print("PRODUCTO:");
+  Serial.println("");
+  Serial.println("");
+  Serial.println("****************");
+  Serial.print("CADUCIDAD:    ");
+  
 
   byte buffer1[18];
 
@@ -292,7 +320,8 @@ void mfrc_read()
     }
   }
   Serial.println(" ");
-  Serial.print("CADUCIDAD:");
+  Serial.print("PRODUCTO:  ");
+  
 
   //---------------------------------------- GET LAST NAME
 
@@ -321,10 +350,44 @@ void mfrc_read()
 
   //----------------------------------------
 
-  Serial.println(F("\n**End Reading**\n"));
+  //Serial.println(F("\n**End Reading**\n"));
 
   delay(1000); //change value if you want to read cards faster
 
   mfrc522.PICC_HaltA();
   mfrc522.PCD_StopCrypto1();
+}
+
+//Echoes the entered data through serial terminal
+byte serial_echo( byte * data)
+{
+  while(!Serial.available() );
+
+
+  //byte data[30] = {}; //serial character buffer
+  //byte len; //number of characters stored in the buffer
+
+  byte  index = 0;
+
+  while( 1  )
+  {
+    
+     while(!Serial.available() );
+    data[index] = Serial.read();
+    Serial.print((char) data[index]);
+     if('.' == (char)data[index])
+    {
+      break;
+    }
+    
+
+    index = index+ 1;
+    index = index %30;
+
+   
+  }
+  Serial.println(" ");
+  Serial.println("DATOS REGISTRADOS");
+  return index;
+
 }
